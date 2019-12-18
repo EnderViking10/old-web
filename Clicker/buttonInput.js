75b22c07cd998cba865bd99a9bc0ff039d79cb9d
Enderviking19 <mrerikk6@gmail.com>
2019-12-18 12:39:45 -0500

    Update buttonInput.js

--- Clicker/buttonInput.js
+++ Clicker/buttonInput.js
@@ -1,34 +1,34 @@
 function button1(){
 	if(money >= 50){
-		inc[0] += .5;
+		inc[0] += 1;
 		money -= 50;
 	}
 }
 
 function button2(){
 	if(money >= 100){
-		inc[1] += .5;
+		inc[1] += .9;
 		money -= 100;
 	}
 }
 
 function button3(){
 	if(money >= 200){
-		inc[2] += .5;
+		inc[2] += .8;
 		money -= 200;
 	}
 }
 
 function button4(){
 	if(money >= 400){
-		inc[3] += .5;
+		inc[3] += .7;
 		money -= 400;
 	}
 }
 
 function button5(){
 	if(money >= 800){
-		inc[4] += .5;
+		inc[4] += .6;
 		money -= 800;
 	}
 }
@@ -42,28 +42,28 @@ function button6(){
 
 function button7(){
 	if(money >= 3200){
-		inc[6] += .5;
+		inc[6] += .4;
 		money -= 3200;
 	}
 }
 
 function button8(){
 	if(money >= 6300){
-		inc[7] += .5;
+		inc[7] += .3;
 		money -= 6400;
 	}
 }
 
 function button9(){
 	if(money >= 12800){
-		inc[8] += .5;
+		inc[8] += .2;
 		money -= 12800;
 	}
 }
 
 function button10(){
 	if(money >= 25600){
-		inc[9] += .5;
+		inc[9] += .1;
 		money -= 25600;
 	}
 }
